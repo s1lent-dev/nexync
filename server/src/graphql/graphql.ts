aa3d61d89c503c93c1cf3f371d7fe523674080b2
s1lent-dev <deshpande.pxresh@gmail.com>
2024-11-22 02:33:43 +0530

    minor fixtures

--- server/src/graphql/graphql.ts
+++ server/src/graphql/graphql.ts
@@ -1,9 +1,11 @@
 import gql from "graphql-tag";
+import { resolve } from "path";
 import { ApolloServer } from "@apollo/server";
 import { buildSubgraphSchema } from "@apollo/subgraph";
 import { readFileSync } from "fs";
 import { resolvers } from "./resolvers/resolvers.js";
-const typeDefs = gql(readFileSync("./dist/graphql/schema/schema.graphql", { encoding: "utf-8" }));
+const schemaPath = resolve(__dirname, "../graphql/schema/schema.graphql");
+const typeDefs = gql(readFileSync(schemaPath, { encoding: "utf-8" }));
 const graphqlServer = new ApolloServer({
     schema: buildSubgraphSchema([
         {
