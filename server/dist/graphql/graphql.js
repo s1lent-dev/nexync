6970cad6ba5e5b199608e0361ec96686e83a68f2
s1lent-dev <deshpande.pxresh@gmail.com>
2024-11-22 02:38:07 +0530

    minor fix

--- server/dist/graphql/graphql.js
+++ server/dist/graphql/graphql.js
@@ -1,9 +1,12 @@
 import gql from "graphql-tag";
-import { resolve } from "path";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from 'url';
 import { ApolloServer } from "@apollo/server";
 import { buildSubgraphSchema } from "@apollo/subgraph";
 import { readFileSync } from "fs";
 import { resolvers } from "./resolvers/resolvers.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const schemaPath = resolve(__dirname, "../graphql/schema/schema.graphql");
 const typeDefs = gql(readFileSync(schemaPath, { encoding: "utf-8" }));
 const graphqlServer = new ApolloServer({
