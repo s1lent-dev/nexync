ea6c92424f1f5bd6b9b2a4f625e79039c1d3c88f
s1lent-dev <deshpande.pxresh@gmail.com>
2024-12-29 14:03:02 +0530

    fix

--- server/src/services/notify.service.ts
+++ server/src/services/notify.service.ts
@@ -3,7 +3,7 @@ import { Message, getMessaging } from "firebase-admin/messaging";
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { FRONTEND_URL } from '../config/config';
+import { FRONTEND_URL } from '../config/config.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
